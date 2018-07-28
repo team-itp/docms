341df5d3d566e4a024c5e5f48f4872063ebd77d3
永野ゆうと <chamelion@hotmail.co.jp>
2018-07-29 06:21:38 +0900

    テストプロジェクトを追加してWatchingFileViewModelのテストを作成

---
+++ src/Docms.Uploader.Tests/FileWatch/WatchingFileViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Docms.Uploader.FileWatch
+{
+    [TestClass]
+    public class WatchingFileViewModelTests
+    {
+        private WatchingFileViewModel sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sut = WatchingFileViewModel.Create("C:\\testdir\\testdir2\\test.PNG", "C:\\testdir");
+        }
+
+        [TestMethod]
+        public void 相対パスが表示名として設定されること()
+        {
+            Assert.AreEqual("testdir2\\test.PNG", sut.DisplayPath);
+        }
+
+        [TestMethod]
+        public void ファイル名が設定されること()
+        {
+            Assert.AreEqual("test.PNG", sut.FileName);
+        }
+
+        [TestMethod]
+        public void 拡張子が小文字で設定されること()
+        {
+            Assert.AreEqual(".png", sut.Extension);
+        }
+
+
+        [TestMethod]
+        public void 絶対パスが設定されること()
+        {
+            Assert.AreEqual("C:\\testdir\\testdir2\\test.PNG", sut.FullPath);
+        }
+    }
+}
---
+++ src/Docms.Uploader.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Docms.Uploader.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Docms.Uploader.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27768946-ff7d-46fa-949b-9890998d4e58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
