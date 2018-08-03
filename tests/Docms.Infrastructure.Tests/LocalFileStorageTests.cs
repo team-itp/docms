99f947dc90a6986c1023236b6c1f1344900f391e
Yuto Nagano <chamelion@hotmail.co.jp>
2018-08-04 00:40:34 +0900

    ローカルファイルストレージのテストを追加

--- tests/Docms.Infrastructure.Tests/LocalFileStorageTests.cs
+++ tests/Docms.Infrastructure.Tests/LocalFileStorageTests.cs
@@ -1,8 +1,8 @@
 ﻿using Docms.Infrastructure.Files;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Docms.Infrastructure.Tests
@@ -75,6 +75,41 @@ namespace Docms.Infrastructure.Tests
             Assert.AreEqual("dir2/content1.txt", file.Path.ToString());
             Assert.AreEqual(12, file.Size);
             Assert.IsNotNull(file.Sha1Hash);
+        }
+
+        [TestMethod]
+        public async Task ファイルのストリームがオープン出来ること()
+        {
+            CreateFile("dir2\\content1.txt", "dir2content1");
+            using (var fs = await sut.OpenAsync("dir2\\content1.txt"))
+            using (var reader = new StreamReader(fs, Encoding.UTF8))
+            {
+                var text = await reader.ReadLineAsync();
+                Assert.AreEqual("dir2content1", text);
+            }
+        }
+
+        [TestMethod]
+        public async Task ストリームからファイルに保存できること()
+        {
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes("dir2content1"));
+            ms.Seek(0, SeekOrigin.Begin);
+            var file = await sut.SaveAsync("dir2\\content1.txt", ms);
+            Assert.AreEqual("dir2content1", System.IO.File.ReadAllText(Path.Combine(basepath, "dir2\\content1.txt")));
+            Assert.AreEqual("dir2/content1.txt", file.Path.ToString());
+            Assert.AreEqual(12, file.Size);
+            Assert.IsNotNull(file.Sha1Hash);
+        }
+
+        [TestMethod]
+        public async Task ファイルを削除できること()
+        {
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes("dir2content1"));
+            ms.Seek(0, SeekOrigin.Begin);
+            await sut.SaveAsync("dir2\\content1.txt", ms);
+            Assert.IsTrue(System.IO.File.Exists(Path.Combine(basepath, "dir2\\content1.txt")));
+            await sut.DeleteFileAsync("dir2\\content1.txt");
+            Assert.IsFalse(System.IO.File.Exists(Path.Combine(basepath, "dir2\\content1.txt")));
         }
     }
 }
