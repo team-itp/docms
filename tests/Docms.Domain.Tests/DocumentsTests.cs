e1443422fbd1011e14ad8ced764d5822702bef5d
Yuto Nagano <chamelion@hotmail.co.jp>
2018-08-04 20:29:44 +0900

    Documents ドメインの最初のテストを作成

---
+++ tests/Docms.Domain.Tests/DocumentsTests.cs
@@ -0,0 +1,16 @@
+﻿using Docms.Domain.Documents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Docms.Domain.Tests
+{
+    [TestClass]
+    public class DocumentsTests
+    {
+        [TestMethod]
+        public void ドキュメントを新規に作成しドキュメントの作成イベントが発生する()
+        {
+            var sut = new Document(new DocumentPath("test.txt"), "text/plain", 10, new byte[] { 1, 2, 3, 4 });
+            Assert.AreEqual(1, sut.DomainEvents.Count);
+        }
+    }
+}
