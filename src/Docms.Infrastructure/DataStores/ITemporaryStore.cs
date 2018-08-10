4c78d86529aef08d3be4a3a188e65c46eb70002a
Yuto Nagano <chamelion@hotmail.co.jp>
2018-08-11 07:40:49 +0900

    メモリーにStreamを保持できるクラスを作成

---
+++ src/Docms.Infrastructure/DataStores/ITemporaryStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Docms.Infrastructure.DataStores
+{
+    public interface ITemporaryStore
+    {
+        Task<DateTime> SaveAsync(Guid id, Stream data);
+        Task<Stream> OpenStreamAsync(Guid id);
+        Task DeleteAsync(Guid id);
+        Task DeleteBeforeAsync(DateTime timestamp);
+    }
+}
