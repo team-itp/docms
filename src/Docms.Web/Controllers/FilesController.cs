14c6e0ff6ccfab5f7691208e909f8c5636fb870a
永野ゆうと <chamelion@hotmail.co.jp>
2018-08-25 06:49:47 +0900

    ファイルの更新をブラウザがしてくれない不具合の修正

--- src/Docms.Web/Controllers/FilesController.cs
+++ src/Docms.Web/Controllers/FilesController.cs
@@ -53,6 +53,7 @@ namespace Docms.Web.Controllers
             }
 
             var file = await _storage.GetEntryAsync(path) as File;
+            Response.Headers.Add("Cache-Control", "max-age=15");
             return File(await file.OpenAsync(), entry.ContentType, entry.Name, new DateTimeOffset(entry.LastModified), new EntityTagHeaderValue("\"" + entry.Hash + "\""));
         }
 
