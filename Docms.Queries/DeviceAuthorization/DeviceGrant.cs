aa693905b60abb4387767e86f8359f9ec1a8d499
永野ゆうと <chamelion@hotmail.co.jp>
2018-09-10 01:22:37 +0900

    Read モデルを作成(コンパイルエラーの修正)

---
+++ Docms.Queries/DeviceAuthorization/DeviceGrant.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Docms.Queries.DeviceAuthorization
+{
+    public class DeviceGrant
+    {
+        [Column("DeviceId")]
+        [Key]
+        public string DeviceId { get; set; }
+        [Column("GrantedBy")]
+        public string GrantedBy { get; set; }
+    }
+}
---
+++ Docms.Queries/DeviceAuthorization/IDeviceGrantsQueries.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Docms.Queries.DeviceAuthorization
+{
+    public interface IDeviceGrantsQueries
+    {
+        Task<bool> IsGrantedAsync(string deviceId);
+    }
+}
