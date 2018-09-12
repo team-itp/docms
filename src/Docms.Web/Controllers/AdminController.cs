fcfc098cbe061583b80dc367f7159c28290c7270
Yuto Nagano <chamelion@hotmail.co.jp>
2018-09-12 17:27:04 +0900

    WIP 端末承認画面

--- src/Docms.Web/Controllers/AdminController.cs
+++ src/Docms.Web/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using Docms.Web.Application.Commands;
+﻿using Docms.Queries.DeviceAuthorization;
+using Docms.Web.Application.Commands;
+using Docms.Web.Application.Identity;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -30,6 +33,30 @@ namespace Docms.Web.Controllers
             var response = await mediator.Send(command);
             TempData["Message"] = "再構築が完了しました。";
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("devices")]
+        public async Task<IActionResult> ListDevices([FromServices] IDeviceGrantsQueries queries)
+        {
+            var devices = await queries.GetDevicesAsync();
+            return View(devices);
+        }
+
+        [HttpPost("devices/grant")]
+        public async Task<IActionResult> GrantDevice(
+            [FromServices] UserManager<ApplicationUser> userManager,
+            [FromServices] IMediator mediator, 
+            [FromForm] string deviceId)
+        {
+            var appUser = await userManager.FindByNameAsync(User.Identity.Name);
+            var command = new GrantDeviceCommand()
+            {
+                DeviceId = deviceId,
+                ByUserId = appUser.Id
+            };
+            var response = await mediator.Send(command);
+            TempData["Message"] = "端末を承認しました。";
+            return RedirectToAction(nameof(ListDevices));
         }
     }
 }
