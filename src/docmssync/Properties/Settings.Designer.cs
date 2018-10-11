46db610b6652ad27a6e9e3733111430dd1e44c41
Yuto Nagano <chamelion@hotmail.co.jp>
2018-10-11 11:08:28 +0900

    設定をユーザー設定からアプリケーション設定に変更

--- src/docmssync/Properties/Settings.Designer.cs
+++ src/docmssync/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace docmssync.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -23,51 +23,39 @@ namespace docmssync.Properties {
             }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("C:\\temp\\Images")]
         public string WatchPath {
             get {
                 return ((string)(this["WatchPath"]));
             }
-            set {
-                this["WatchPath"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:51693")]
         public string ServerUrl {
             get {
                 return ((string)(this["ServerUrl"]));
             }
-            set {
-                this["ServerUrl"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("testuser")]
         public string UploadUserName {
             get {
                 return ((string)(this["UploadUserName"]));
             }
-            set {
-                this["UploadUserName"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("Passw0rd")]
         public string UploadUserPassword {
             get {
                 return ((string)(this["UploadUserPassword"]));
-            }
-            set {
-                this["UploadUserPassword"] = value;
             }
         }
     }
