bcc914781f8673a5d4ac99d0a0a4d29199820427
Yuto Nagano <chamelion@hotmail.co.jp>
2018-10-31 21:35:56 +0900

    一時ファイルを使用した TempData の作成

--- src/Docms.Infrastructure/DataStores/TemporaryStore.cs
+++ src/Docms.Infrastructure/DataStores/TemporaryStore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -8,17 +9,13 @@ namespace Docms.Infrastructure.DataStores
     {
         public async Task<ITempData> CreateAsync(Stream stream, long sizeOfStream)
         {
-            if (sizeOfStream == -1)
+            if (sizeOfStream > -1 && sizeOfStream < 33_554_432)
             {
-                MemoryStream ms = new MemoryStream();
-                await stream.CopyToAsync(ms);
-                return new InMemoryTempData(ms.ToArray(), ms.Length);
+                return await InMemoryTempData.Create(stream, sizeOfStream);
             }
             else
             {
-                MemoryStream ms = new MemoryStream((int)sizeOfStream);
-                await stream.CopyToAsync(ms);
-                return new InMemoryTempData(ms.ToArray(), sizeOfStream);
+                return await FileTempData.Create(stream);
             }
         }
 
@@ -34,9 +31,9 @@ namespace Docms.Infrastructure.DataStores
         private bool disposedValue;
         private byte[] data;
 
-        public InMemoryTempData(byte[] data, long SizeOfStream)
+        public InMemoryTempData(byte[] data)
         {
-            SizeOfData = SizeOfStream;
+            SizeOfData = data.Length;
             this.data = data;
         }
 
@@ -55,6 +52,68 @@ namespace Docms.Infrastructure.DataStores
         {
             data = null;
             disposedValue = true;
+        }
+
+        internal static async Task<ITempData> Create(Stream stream, long sizeOfStream)
+        {
+            MemoryStream ms = new MemoryStream((int)sizeOfStream);
+            await stream.CopyToAsync(ms);
+            return new InMemoryTempData(ms.ToArray());
+        }
+    }
+
+    public sealed class FileTempData : ITempData
+    {
+        private bool disposedValue;
+        private FileInfo fileInfo;
+        private List<Stream> streams = new List<Stream>();
+
+        private FileTempData(FileInfo fileInfo)
+        {
+            this.fileInfo = fileInfo;
+        }
+
+        internal static async Task<FileTempData> Create(Stream stream)
+        {
+            var tempPath = Path.GetTempFileName();
+            var fi = new FileInfo(tempPath);
+            using (var fs = fi.OpenWrite())
+            {
+                await stream.CopyToAsync(fs);
+            }
+            return new FileTempData(fi);
+        }
+
+        public long SizeOfData => fileInfo.Length;
+
+        public Task<Stream> OpenStreamAsync()
+        {
+            if (disposedValue)
+            {
+                throw new InvalidOperationException();
+            }
+            var stream = fileInfo.OpenRead();
+            streams.Add(stream);
+            return Task.FromResult<Stream>(stream);
+        }
+
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                disposedValue = true;
+                try
+                {
+                    streams.ForEach(s =>
+                    {
+                        s.Close();
+                        s.Dispose();
+                    });
+                    streams.Clear();
+                    fileInfo.Delete();
+                }
+                catch { }
+            }
         }
     }
 }
