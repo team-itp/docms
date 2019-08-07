9c3014bcdc1f37524af4437b34af09ad03975b93
Yuto Nagano <chamelion@hotmail.co.jp>
2019-08-08 01:07:49 +0900

    ハッシュをGitと同じ計算方法にする

---
+++ src/Docms.Domain/Documents/Hash.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Docms.Domain.Documents
+{
+    public class Hash
+    {
+        public string Value { get; }
+
+        protected Hash(byte[] value)
+        {
+            if (value.Length != 20)
+            {
+                throw new ArgumentException(nameof(value));
+            }
+            Value = BitConverter.ToString(value).Replace("-", "").ToLowerInvariant();
+        }
+
+        public Hash(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public static Hash CalculateHash(byte[] data)
+        {
+            return CalculateHash(new MemoryStream(data));
+        }
+
+        public static Hash CalculateHash(Stream stream)
+        {
+            using (var sha1 = new SHA1CryptoServiceProvider())
+            {
+                var header = Encoding.ASCII.GetBytes($"blob {stream.Length}\0");
+                sha1.TransformBlock(header, 0, header.Length, null, 0);
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                do
+                {
+                    bytesRead = stream.Read(buffer, 0, 4096);
+                    if (bytesRead > 0)
+                    {
+                        sha1.TransformBlock(buffer, 0, bytesRead, null, 0);
+                    }
+                } while (bytesRead > 0);
+                sha1.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+                return new Hash(sha1.Hash);
+            }
+        }
+    }
+}
---
+++ tests/Docms.Domain.Tests/HashTests.cs
@@ -0,0 +1,18 @@
+﻿using Docms.Domain.Documents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Docms.Domain.Tests
+{
+    [TestClass]
+    public class HashTests
+    {
+        [TestMethod]
+        public void データからSHA1ハッシュを取得する()
+        {
+            var content = Encoding.ASCII.GetBytes("what is up, doc?");
+            var sut = Hash.CalculateHash(content);
+            Assert.AreEqual("bd9dbf5aae1a3862dd1526723246b20206e5fc37", sut.ToString());
+        }
+    }
+}
