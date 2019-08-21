376cd5f5205f03313a75a6930de954ff22e8515c
Yuto Nagano <chamelion@hotmail.co.jp>
2019-08-22 06:32:14 +0900

    ユーザーの実装イメージを追加

---
+++ src/Docms.Domain/Users/IUser.cs
@@ -0,0 +1,10 @@
+﻿using Docms.Domain.Core;
+
+namespace Docms.Domain.Users
+{
+    public interface IUser
+    {
+        UserId Id { get; }
+        string Name { get; }
+    }
+}
---
+++ src/Docms.Domain/Users/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Docms.Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task<IUser> FindByIdAsync(string id);
+        Task Register(IUser user);
+    }
+}
