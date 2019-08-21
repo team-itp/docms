9d1cddb6f6434857211a0786043a69f103dec8ed
Yuto Nagano <chamelion@hotmail.co.jp>
2019-08-22 06:31:06 +0900

    ID項目をVOとして追加

---
+++ src/Docms.Domain/Core/DeviceId.cs
@@ -0,0 +1,26 @@
+﻿using Docms.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+
+namespace Docms.Domain.Core
+{
+    public class DeviceId : ValueObject
+    {
+        public Guid Value { get; }
+
+        public DeviceId()
+        {
+            Value = Guid.NewGuid();
+        }
+
+        public DeviceId(Guid value)
+        {
+            Value = value;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+}
---
+++ src/Docms.Domain/Core/UserId.cs
@@ -0,0 +1,26 @@
+﻿using Docms.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+
+namespace Docms.Domain.Core
+{
+    public class UserId : ValueObject
+    {
+        public Guid Value { get; }
+
+        public UserId()
+        {
+            Value = Guid.NewGuid();
+        }
+
+        public UserId(Guid value)
+        {
+            Value = value;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+    }
+}
