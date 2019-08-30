d0e83af6ef06502a0758ad57bded01e82fba2782
_永野 有音 <o_nagano@dcr.local>
2019-08-30 10:50:39 +0900

    react に material-ui を導入

--- src/docms-web-frontend/src/App.js
+++ src/docms-web-frontend/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blueGrey, orange } from '@material-ui/core/colors';
+import { ThemeProvider } from '@material-ui/styles';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blueGrey['800'],
+    },
+    secondary: {
+      main: orange['700'],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              文書管理システム
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
 
