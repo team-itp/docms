784a415cae7cd0ee066250c649c298c29173dd9a
Yuto Nagano <chamelion@hotmail.co.jp>
2019-09-07 01:36:19 +0900

    メンテナンス用のプロジェクトを作成

---
+++ src/Docms.Maintainance.CleanupTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Docms.Maintainance.CleanupTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
