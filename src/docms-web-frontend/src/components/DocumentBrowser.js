2421e7b755454980ece62d8b92cef21d2db50482
_永野 有音 <o_nagano@dcr.local>
2019-09-06 19:06:51 +0900

    パンくずリストのプロトタイプを更新

--- src/docms-web-frontend/src/components/DocumentBrowser.js
+++ src/docms-web-frontend/src/components/DocumentBrowser.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import { Breadcrumbs, Container, Link, Typography, Toolbar } from '@material-ui/core';
 
-class DocumentBrowser extends React.Component {
+function DocumentBrowser() {
+  return (
+    <Container maxWidth="xl">
+      <Toolbar>
+        <DocumentHeader />
+      </Toolbar>
+    </Container>
+  );
+}
+
+class DocumentHeader extends React.Component {
   render() {
     return (
-      <Container maxWidth="xl">
-        <h1>DocumentBrowser</h1>
-      </Container>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link color="inherit" component={RouterLink} to="/">
+          Home
+          </Link>
+        <Link color="inherit" component={RouterLink} to="/Catalog">
+          Catalog
+          </Link>
+        <Link color="inherit" component={RouterLink} to="/Catalog/Accessories">
+          Accessories
+          </Link>
+        <Link color="inherit" component={RouterLink} to="/Catalog/Accessories/New Collection">
+          New Collection
+          </Link>
+        <Typography color="textPrimary">Belts</Typography>
+      </Breadcrumbs>
     );
   }
 }
