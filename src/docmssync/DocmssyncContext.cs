608d7e8ea81fb8d84aeff44a1248067b239b96b7
Yuto Nagano <chamelion@hotmail.co.jp>
2019-11-09 15:00:30 +0900

    トレイ常駐のプロジェクトを追加

---
+++ src/docmssync/DocmssyncContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace docmssync
+{
+    class DocmssyncContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public DocmssyncContext()
+        {
+            var icon = new Icon(this.GetType().Assembly.GetManifestResourceStream("docmssync.icon.ico"));
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("終了", null, (s, e) =>
+            {
+                trayIcon.Visible = false;
+                Application.Exit();
+            });
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = icon,
+                ContextMenuStrip = contextMenu,
+                Visible = true
+            };
+        }
+    }
+}
